45010a514a1f3f181aa0590e49b45b31abc0f366
admomin <67753387+admomin@users.noreply.github.com>
2024-03-13 02:16:32 -0700

    Update eidoffer.js

--- eidoffer.js
+++ eidoffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://smrturl.co/o/597804/53385229?s1=gpmama";
-var bllink = "https://smrturl.co/o/597804/53385239?s1=rumma";
+var gplink = "https://flibblix.com/cl/5df389eab000c37f?p1=&p2=&source=YouTube&site=mygpa";
+var bllink = "https://flibblix.com/cl/6dafda6602ce254d?p1=&p2=&source=YouTube&site=myblo";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=rumma";
-var rblink = "https://smrturl.co/o/597804/53385239?s1=rumma";
+var rblink = "https://flibblix.com/cl/e5ac2b44ad5cef3b?p1=&p2=&source=YouTube&site=myorobi";
