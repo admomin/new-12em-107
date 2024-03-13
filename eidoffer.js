f59098bd52e3ab0a0bf94101081f8a2b95f7a5be
admomin <67753387+admomin@users.noreply.github.com>
2024-03-13 11:36:56 +0600

    Create eidoffer.js

---
+++ eidoffer.js
@@ -0,0 +1,4 @@
+var gplink = "https://flibblix.com/cl/1f75dc63e7e9e1c8?p1=&p2=&source=Instagram&site=M2";
+var bllink = "https://zypholuxa.com/cl/792d67aa9e8d6ce9?p1=&p2=&source=YouTube&site=mybal";
+var arlink = "https://smrturl.co/o/597804/53456437?s1=muminviaya";
+var rblink = "https://flibblix.com/cl/a83ffd2edbee9c12?p1=&p2=&source=instagram&site=MominRobi";
