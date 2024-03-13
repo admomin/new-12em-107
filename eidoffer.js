728866243241cc793c1903740ec13fc445807eb1
admomin <67753387+admomin@users.noreply.github.com>
2024-03-13 03:27:53 -0700

    Update eidoffer.js

--- eidoffer.js
+++ eidoffer.js
@@ -1,4 +1,4 @@
 var gplink = "https://smrturl.co/o/597804/53385229?s1=gp";
 var bllink = "https://smrturl.co/o/597804/53385239?s1=all";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=all";
-var rblink = "https://smrturl.co/o/597804/53385239?s1=all";
+var rblink = "https://zyvilix.com/cl/79dcba02db027012?p1=&p2=&source=instagram&site=Momin";
