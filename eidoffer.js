3872736f83d9b8fb44351cc418505d5e026d4702
admomin <67753387+admomin@users.noreply.github.com>
2024-03-13 03:52:30 -0700

    Update eidoffer.js

--- eidoffer.js
+++ eidoffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://smrturl.co/o/597804/53385229?s1=gp";
-var bllink = "https://smrturl.co/o/597804/53385239?s1=all";
+var gplink = "https://zyvilix.com/cl/469d081402ac1950?p1=&p2=&source=instagram&site=newGP";
+var bllink = "https://zyvilix.com/cl/fc86f98cfcd3e414?p1=&p2=&source=instagram&site=newBanglalink";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=all";
-var rblink = "https://zyvilix.com/cl/79dcba02db027012?p1=&p2=&source=instagram&site=Momin";
+var rblink = "https://zyvilix.com/cl/1d74cdf70497c6e7?p1=&p2=&source=instagram&site=newRobi";
