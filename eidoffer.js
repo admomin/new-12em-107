576dd1ee3d6ac780d1dadf31674f9ade5d0654be
admomin <67753387+admomin@users.noreply.github.com>
2024-03-13 08:41:30 -0700

    Update eidoffer.js

--- eidoffer.js
+++ eidoffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://plivexx.com/cl/0380296ae2a8f8e7?p1=&p2=&source=Instagram&site=MBGP";
+var gplink = "https://snozxara.com/cl/deed994b9536f348?p1=&p2=&source=instagram&site=ff";
 var bllink = "https://plivexx.com/cl/f5c1dc7bf84d05ed?p1=&p2=&source=instagram&site=DD";
 var arlink = "https://aff.26march.shop/?utm_medium=15d7e170b055c9bea26c38a60ecc113a76199cb4&utm_campaign=ggtomonitizarb";
 var rblink = "https://zyvilix.com/cl/1d74cdf70497c6e7?p1=&p2=&source=instagram&site=newRobi";
