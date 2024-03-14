9636e23688b5161ec66b97a9b661132b16df29b9
admomin <67753387+admomin@users.noreply.github.com>
2024-03-13 21:34:13 -0700

    Update eidoffer.js

--- eidoffer.js
+++ eidoffer.js
@@ -1,4 +1,4 @@
 var gplink = "https://flibblix.com/cl/68788dce8de719a9?p1=&p2=&source=FACEBOOK&site=MURADGP1";
 var bllink = "https://snozxara.com/cl/4ce5984d6fae9345?p1=&p2=&source=instagram&site=EidBal";
 var arlink = "https://aff.26march.shop/?utm_medium=a68bee459a25cd42e0fe4947cdc89a4763341301&utm_campaign=ridairtel";
-var rblink = "https://snozxara.com/cl/01a76298b7e1eb1c?p1=&p2=&source=instagram&site=EidRobi";
+var rblink = "https://snozxara.com/cl/90e6715ad2667e34?p1=&p2=&source=instagram&site=MyRobi";
