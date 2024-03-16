9fa636084896e8f7797b037d67903d2516a2fb3a
admomin <67753387+admomin@users.noreply.github.com>
2024-03-16 03:08:22 -0700

    Update eidoffer.js

--- eidoffer.js
+++ eidoffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://aff.26march.shop/?utm_medium=7b313f4a000227dbf7cde5a6570688643e2f9a0c&utm_campaign=SagorGP";
-var bllink = "https://aff.26march.shop/?utm_medium=81a47b0804cbc920ffc0aade4064dd9e7ac04dc1&utm_campaign=SagorBL";
+var gplink = "https://fynddyx.com/cl/3400fe696dce849b?p1=&p2=&source=instagram&site=gp";
+var bllink = "https://fynddyx.com/cl/2b8a19adae590df4?p1=&p2=&source=instagram&site=xbal";
 var arlink = "https://aff.26march.shop/?utm_medium=c782a06c3e0a0dd81b2d2640ed760445190b1461&utm_campaign=SagorAR";
-var rblink = "https://aff.26march.shop/?utm_medium=c782a06c3e0a0dd81b2d2640ed760445190b1461&utm_campaign=SagorAR";
+var rblink = "https://fynddyx.com/cl/df4a76c410757e62?p1=&p2=&source=instagram&site=myrobix";
