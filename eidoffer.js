a1283863e74f00027e9f0d924ce92dbc0c83b2b4
admomin <67753387+admomin@users.noreply.github.com>
2024-03-16 09:48:49 -0700

    Update eidoffer.js

--- eidoffer.js
+++ eidoffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://fynddyx.com/cl/3400fe696dce849b?p1=&p2=&source=instagram&site=gp";
-var bllink = "https://fynddyx.com/cl/2b8a19adae590df4?p1=&p2=&source=instagram&site=xbal";
+var gplink = "https://quttyvex.com/cl/c776d14d30915163?p1=&p2=&source=instagra,&site=sexgp";
+var bllink = "https://quttyvex.com/cl/20d5b12b56c16fde?p1=&p2=&source=instagram&site=sexbal";
 var arlink = "https://aff.26march.shop/?utm_medium=c782a06c3e0a0dd81b2d2640ed760445190b1461&utm_campaign=SagorAR";
-var rblink = "https://fynddyx.com/cl/df4a76c410757e62?p1=&p2=&source=instagram&site=myrobix";
+var rblink = "https://quttyvex.com/cl/9f03f50941b7523b?p1=&p2=&source=instagram&site=sexrobi";
