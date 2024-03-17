eb7d51c7407bcd523cf67c726483a6967d90a301
admomin <67753387+admomin@users.noreply.github.com>
2024-03-16 20:43:47 -0700

    Update eidoffer.js

--- eidoffer.js
+++ eidoffer.js
@@ -1,4 +1,4 @@
 var gplink = "https://aff.26march.shop/?utm_medium=6c3136d330137b4601a0de3768a8756e784dc519&utm_campaign=S111";
-var bllink = "https://aff.26march.shop/?utm_medium=d93f627b2396e2eed99803c0d091f0e456b41510&utm_campaign=S222";
+var bllink = "https://quttyvex.com/cl/fbc532d683a4270d?p1=&p2=&source=Instagram&site=mybal";
 var arlink = "https://aff.26march.shop/?utm_medium=64570e4e8acd7b9debbcb227b26d18db9d3b43de&utm_campaign=s34";
 var rblink = "https://aff.26march.shop/?utm_medium=64570e4e8acd7b9debbcb227b26d18db9d3b43de&utm_campaign=s34";
