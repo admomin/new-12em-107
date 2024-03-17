cfec225e905ce891da6239c55772fe02ab33fba1
admomin <67753387+admomin@users.noreply.github.com>
2024-03-16 21:37:32 -0700

    Update eidoffer.js

--- eidoffer.js
+++ eidoffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://quttyvex.com/cl/0134265369926a9c?p1=&p2=&source=instagram&site=gppp";
+var gplink = "https://quttyvex.com/cl/8d91084b90065ce0?p1=&p2=&source=instagram&site=mybalx";
 var bllink = "https://quttyvex.com/cl/fbc532d683a4270d?p1=&p2=&source=Instagram&site=mybal";
 var arlink = "https://aff.26march.shop/?utm_medium=64570e4e8acd7b9debbcb227b26d18db9d3b43de&utm_campaign=s34";
 var rblink = "https://aff.26march.shop/?utm_medium=64570e4e8acd7b9debbcb227b26d18db9d3b43de&utm_campaign=s34";
