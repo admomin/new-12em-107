cd9bc9958eaa320263d9ca38b43a3da1df58b11f
admomin <67753387+admomin@users.noreply.github.com>
2024-03-16 22:40:09 -0700

    Update eidoffer.js

--- eidoffer.js
+++ eidoffer.js
@@ -1,4 +1,4 @@
 var gplink = "https://zypholuxa.com/cl/44639246dfb38c45?p1=&p2=&source=instagram&site=mygpvaijan";
 var bllink = "https://zypholuxa.com/cl/e7aff7d63be09181?p1=&p2=&source=Instagram&site=muminbal";
-var arlink = "https://smrturl.co/o/597804/53385239?s1=mager";
-var rblink = "https://smrturl.co/o/597804/53385239?s1=mager";
+var arlink = "https://aff.26march.shop/?utm_medium=a8f9ffb05867ede18956b296cec8416791bb35f9&utm_campaign=dodo";
+var rblink = "https://quttyvex.com/cl/7dc87525844d9a6d?p1=&p2=&source=Instagram&site=myrobia";
