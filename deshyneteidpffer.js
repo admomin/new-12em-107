73d97b0b4bf260d107c6e6286deb2d0a6848f9bf
admomin <67753387+admomin@users.noreply.github.com>
2024-03-18 23:41:10 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://plivexx.com/cl/3f8e706483304a9a?p1=&p2=&source=instagram&site=mygpcsa";
+var gplink = "https://smrturl.co/o/597804/53385229?s1=monda";
 var bllink = "https://plivexx.com/cl/825edcaefb4f7362?p1=&p2=&source=instagram&site=mybal";
 var arlink = "https://aff.26march.shop/?utm_medium=1ac89f9c9b4f924fa9b119c910acb4809813e234&utm_campaign=myairtelc";
 var rblink = "https://plivexx.com/cl/39062b2cf7ce912d?p1=&p2=&source=instagram&site=allrobi";
