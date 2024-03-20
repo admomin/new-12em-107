2d937268ccd236cd0728493f40bf8a0c6cfd79ca
admomin <67753387+admomin@users.noreply.github.com>
2024-03-20 07:40:07 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
 var gplink = "https://smrturl.co/o/597804/53385229?s1=balda";
-var bllink = "https://smrturl.co/o/597804/53385239?s1=abal20";
-var arlink = "https://smrturl.co/o/597804/53385239?s1=abal20";
-var rblink = "https://smrturl.co/o/597804/53385239?s1=abal20";
+var bllink = "https://smrturl.co/o/597804/53385239?s1=cd";
+var arlink = "https://smrturl.co/o/597804/53385239?s1=cd";
+var rblink = "https://smrturl.co/o/597804/53385239?s1=cd";
