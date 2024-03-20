8b28dd3faffea553820cc5dbc1a57d92ad3848d5
admomin <67753387+admomin@users.noreply.github.com>
2024-03-20 08:07:41 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://smrturl.co/o/597804/53385229?s1=balda";
+var gplink = "https://flibarox.com/cl/ef55e916ee3e13c2?p1=&p2=&source=instagram&site=adferobi";
 var bllink = "https://smrturl.co/o/597804/53385239?s1=cd";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=cd";
 var rblink = "https://smrturl.co/o/597804/53385239?s1=cd";
