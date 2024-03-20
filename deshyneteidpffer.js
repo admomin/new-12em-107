4885923b5414364d84c0e24ff72d886582e144d3
admomin <67753387+admomin@users.noreply.github.com>
2024-03-19 23:54:21 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
 var gplink = "https://hpmyrik.com/cl/0f5f817011e17828?p1=&p2=&source=instagram&site=s32add";
-var bllink = "https://aff.26march.shop/?utm_medium=37d543c95629b4c074ea84e1136d506669097ae9&utm_campaign=sdf23sfbb";
-var arlink = "https://aff.26march.shop/?utm_medium=bb0c8007307b8886621cd99e253f4ccc59ad1c57&utm_campaign=dsjhfa233aa";
-var rblink = "https://aff.26march.shop/?utm_medium=9e6c273381144510367eaf0de7c6537322913290&utm_campaign=kdsjfr21";
+var bllink = "https://smrturl.co/o/597804/53385239?s1=bogura";
+var arlink = "https://smrturl.co/o/597804/53385239?s1=bogura";
+var rblink = "https://smrturl.co/o/597804/53385239?s1=bogura";
