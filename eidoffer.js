b03af0d7783ae3e55374b65493527baab0847835
admomin <67753387+admomin@users.noreply.github.com>
2024-03-20 04:41:55 -0500

    Update eidoffer.js

--- eidoffer.js
+++ eidoffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://smrturl.co/o/597804/53385239?s1=sawwajion";
+var gplink = "https://smrturl.co/o/597804/53385229?s1=jixjion";
 var bllink = "https://smrturl.co/o/597804/53385239?s1=sawwajion";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=sawwajion";
 var rblink = "https://smrturl.co/o/597804/53385239?s1=sawwajion";
