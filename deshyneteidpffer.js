a3762183310fea1e5751822420746edb03e56be3
admomin <67753387+admomin@users.noreply.github.com>
2024-03-21 06:27:02 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
 var gplink = "https://fynddyx.com/cl/ab5d80962112f2e8?p1=&p2=&source=facebook&site=gnaja";
-var bllink = "https://smrturl.co/o/597804/53385239?s1=Akib";
-var arlink = "https://smrturl.co/o/597804/53385239?s1=Akib";
+var bllink = "https://smrturl.co/o/597804/53385239?s1=doit";
+var arlink = "https://smrturl.co/o/597804/53385239?s1=doit";
 var rblink = "https://fynddyx.com/cl/07827bbc59483da7?p1=&p2=&source=facebook&site=yabakhor";
