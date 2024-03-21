05dac0685e1b3b46e8ae5f1241ab3dc4d567a828
admomin <67753387+admomin@users.noreply.github.com>
2024-03-21 06:05:06 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://hpmyrik.com/cl/c7036dee7dd298f2?p1=&p2=&source=youtube&site=gopala";
+var gplink = "https://fynddyx.com/cl/ab5d80962112f2e8?p1=&p2=&source=facebook&site=gnaja";
 var bllink = "https://smrturl.co/o/597804/53385239?s1=Akib";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=Akib";
-var rblink = "https://hpmyrik.com/cl/0d4a4c0c2643dbe7?p1=&p2=&source=youtube&site=rohomaniya";
+var rblink = "https://fynddyx.com/cl/07827bbc59483da7?p1=&p2=&source=facebook&site=yabakhor";
