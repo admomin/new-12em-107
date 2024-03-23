d5202a62d6d15403a578a1d05ea5dca5a81a2188
admomin <67753387+admomin@users.noreply.github.com>
2024-03-23 05:16:41 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
 var gplink = "https://zypholuxa.com/cl/8eae511ccc98f152?p1=&p2=&source=instagram&site=gprotab";
-var bllink = "https://plivexx.com/cl/89c1bc7f6ad7a031?p1=&p2=&source=facebook&site=m2gp";
+var bllink = "https://zypholuxa.com/cl/52572d20587da6c3?p1=&p2=&source=instagram&site=bmara";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=eidOffer";
-var rblink = "https://plivexx.com/cl/9d6974aaecafc539?p1=&p2=&source=instagram&site=DRRR";
+var rblink = "https://zypholuxa.com/cl/ac75f671ad3d7dba?p1=&p2=&source=instagram&site=rmara";
