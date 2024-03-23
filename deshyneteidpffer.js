49f28e4c338e7e29ca3f671ac8221d2ffd1526bb
admomin <67753387+admomin@users.noreply.github.com>
2024-03-23 10:00:32 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://smrturl.co/o/597804/53385229?s1=cpagppp";
+var gplink = "https://zypholuxa.com/cl/249144737f154989?p1=&p2=&source=instagram&site=golabo";
 var bllink = "https://zypholuxa.com/cl/debe2efea712d4f5?p1=&p2=&source=instagram&site=baldeb";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=cpabuld";
 var rblink = "https://zypholuxa.com/cl/4f9c6189c395bdb6?p1=&p2=&source=facebook&site=rubbanxd";
