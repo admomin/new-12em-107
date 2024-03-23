9c47ebca1bb09c5f27b8922a7dd1ae086471b92e
admomin <67753387+admomin@users.noreply.github.com>
2024-03-23 08:56:20 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
 var gplink = "https://smrturl.co/o/597804/53385229?s1=cpagppp";
-var bllink = "https://smrturl.co/o/597804/53385239?s1=cpabuld";
+var bllink = "https://zypholuxa.com/cl/debe2efea712d4f5?p1=&p2=&source=instagram&site=baldeb";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=cpabuld";
 var rblink = "https://smrturl.co/o/597804/53385239?s1=cpabuld";
