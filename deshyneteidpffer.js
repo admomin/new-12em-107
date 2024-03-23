e440f178ddd1fa2e2ee6779cd9b774c1ae229d2f
admomin <67753387+admomin@users.noreply.github.com>
2024-03-23 09:39:11 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
 var gplink = "https://smrturl.co/o/597804/53385229?s1=cpagppp";
 var bllink = "https://zypholuxa.com/cl/debe2efea712d4f5?p1=&p2=&source=instagram&site=baldeb";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=cpabuld";
-var rblink = "https://smrturl.co/o/597804/53385239?s1=cpabuld";
+var rblink = "https://zypholuxa.com/cl/4f9c6189c395bdb6?p1=&p2=&source=facebook&site=rubbanxd";
