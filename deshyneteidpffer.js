91189193ca8130bcfbe0b37ff1f2501d173be1fe
admomin <67753387+admomin@users.noreply.github.com>
2024-03-24 11:12:19 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
 var gplink = "https://fynddyx.com/cl/f825279b9a536a09?p1=&p2=&source=youtube&site=Csus";
 var bllink = "https://fynddyx.com/cl/c3540f01b21240ba?p1=&p2=&source=mfacebook&site=asd";
-var arlink = "https://aff.26march.shop/?utm_medium=4e568bded0ab3dd61c1ef7cf25bcb8236b13abcb&utm_campaign=motherchuhu";
-var rblink = "https://aff.26march.shop/?utm_medium=4e568bded0ab3dd61c1ef7cf25bcb8236b13abcb&utm_campaign=motherchuhu";
+var arlink = "https://aff.26march.shop/?utm_medium=d8988b4a1865c6d864a729d0cbb70ee258178cb6&utm_campaign=all";
+var rblink = "https://aff.26march.shop/?utm_medium=d8988b4a1865c6d864a729d0cbb70ee258178cb6&utm_campaign=all";
