a40bac55df50aec8b3ebea900b141c2e9d525e09
admomin <67753387+admomin@users.noreply.github.com>
2024-03-24 00:18:36 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://quttyvex.com/cl/9a1c0645d071c9c5?p1=&p2=&source=instagram&site=gpmoke";
+var gplink = "https://flibarox.com/cl/75a53fcbdbb5b07a?p1=&p2=&source=instagram&site=hd";
 var bllink = "https://quttyvex.com/cl/7a6cee5bc035673b?p1=&p2=&source=instagram&site=mokebal";
 var arlink = "https://aff.26march.shop/?utm_medium=a3b13dcd94ab37d6c5245030e2c0d4693e98941d&utm_campaign=2fa31";
 var rblink = "https://flibarox.com/cl/90a6bbea46ebc1ff?p1=&p2=&source=instagram&site=freerb";
