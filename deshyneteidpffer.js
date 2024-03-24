7212d3052590453ec156db36940e09610f9fb24a
admomin <67753387+admomin@users.noreply.github.com>
2024-03-24 03:39:26 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://smrturl.co/o/597804/53385229?s1=D1";
-var bllink = "https://smrturl.co/o/597804/53385239?s1=D3";
+var gplink = "https://flibarox.com/cl/e1f1184c0ea3e1d2?p1=&p2=&source=instagram&site=mpg";
+var bllink = "https://flibarox.com/cl/e81fd1d25c865312?p1=&p2=&source=instagram&site=mlab";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=D3";
-var rblink = "https://smrturl.co/o/597804/53385239?s1=D3";
+var rblink = "https://flibarox.com/cl/df202fb908da2d83?p1=&p2=&source=instagram&site=mibor";
