4f49c9472ae606f43cfd38c3b54c3f3ea20b88ba
admomin <67753387+admomin@users.noreply.github.com>
2024-03-24 19:36:31 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://hpmyrik.com/cl/0b7a225dd5508197?p1=&p2=&source=youtube&site=mumingpp";
-var bllink = "https://hpmyrik.com/cl/60d2994b624d37e1?p1=&p2=&source=youtube&site=muminbal";
+var gplink = "https://hpmyrik.com/cl/d8b84ff0b4c2136c?p1=&p2=&source=facebook&site=toppgg";
+var bllink = "https://hpmyrik.com/cl/a7b631725a48b73b?p1=&p2=&source=facebook&site=topbalxx";
 var arlink = "https://aff.26march.shop/?utm_medium=d8988b4a1865c6d864a729d0cbb70ee258178cb6&utm_campaign=all";
-var rblink = "https://hpmyrik.com/cl/720c2cfb327a7dfc?p1=&p2=&source=youtube&site=muminrobi";
+var rblink = "https://hpmyrik.com/cl/343219a0494ecfa6?p1=&p2=&source=facebook&site=trobi";
