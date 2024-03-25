95b32475ef72f111654310dcdc68e6be3944d1f6
admomin <67753387+admomin@users.noreply.github.com>
2024-03-25 07:36:21 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://hpmyrik.com/cl/dbff01d055318578?p1=&p2=&source=instagram&site=myggo";
-var bllink = "https://plivexx.com/cl/15fdd5278c9590d0?p1=&p2=&source=instagram&site=mdsdaf";
+var gplink = "https://zyvilix.com/cl/fc4719784061f533?p1=&p2=&source=tiktok&site=m11";
+var bllink = "https://zyvilix.com/cl/332607d64861138b?p1=&p2=&source=tiktok&site=m2m";
 var arlink = "https://aff.26march.shop/?utm_medium=d8988b4a1865c6d864a729d0cbb70ee258178cb6&utm_campaign=all";
 var rblink = "https://zyvilix.com/cl/26237e3ce7664694?p1=&p2=&source=instagram&site=mybaskdf";
