871b47ace1050920e06d8c9fc4b9ef5468d59da8
admomin <67753387+admomin@users.noreply.github.com>
2024-03-24 17:08:00 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://fynddyx.com/cl/0da7280ddcd036f5?p1=&p2=&source=youtube&site=hgpmay";
-var bllink = "https://fynddyx.com/cl/366e8b0088fd6d65?p1=&p2=&source=youtube&site=hbalzx";
+var gplink = "https://hpmyrik.com/cl/0b7a225dd5508197?p1=&p2=&source=youtube&site=mumingpp";
+var bllink = "https://hpmyrik.com/cl/60d2994b624d37e1?p1=&p2=&source=youtube&site=muminbal";
 var arlink = "https://aff.26march.shop/?utm_medium=d8988b4a1865c6d864a729d0cbb70ee258178cb6&utm_campaign=all";
-var rblink = "https://fynddyx.com/cl/effefcbe2134a457?p1=&p2=&source=youtube&site=robiya";
+var rblink = "https://hpmyrik.com/cl/720c2cfb327a7dfc?p1=&p2=&source=youtube&site=muminrobi";
