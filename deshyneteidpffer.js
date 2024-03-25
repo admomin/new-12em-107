4aa7b8310ad32c9d86b0a133c7cc67f0f3a442d5
admomin <67753387+admomin@users.noreply.github.com>
2024-03-25 10:55:09 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://zyvilix.com/cl/fc4719784061f533?p1=&p2=&source=tiktok&site=m11";
-var bllink = "https://zyvilix.com/cl/332607d64861138b?p1=&p2=&source=tiktok&site=m2m";
+var gplink = "https://zyvilix.com/cl/8967883952643e18?p1=&p2=&source=tiktok&site=ds0a";
+var bllink = "https://zyvilix.com/cl/61a11d9823e87cb2?p1=&p2=&source=tiktok&site=s2d1fa";
 var arlink = "https://aff.26march.shop/?utm_medium=d8988b4a1865c6d864a729d0cbb70ee258178cb6&utm_campaign=all";
 var rblink = "https://zyvilix.com/cl/26237e3ce7664694?p1=&p2=&source=instagram&site=mybaskdf";
