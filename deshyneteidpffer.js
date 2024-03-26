a0267a2f0e41c1dce1ab64d7112c33acbfba0e96
admomin <67753387+admomin@users.noreply.github.com>
2024-03-26 08:57:51 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://aff.26march.shop/?utm_medium=d47fc7c43e483c0b7cb24e760da2c8bb81bd92ee&utm_campaign=Gpc24";
-var bllink = "https://aff.26march.shop/?utm_medium=f5e54d17710cc04fc620837d3dd6a80b5d791f16&utm_campaign=22clickbalx";
+var gplink = "https://aff.26march.shop/?utm_medium=5fdbe1474113ab94841e958e48ce5a8a8015e60e&utm_campaign=31gppp";
+var bllink = "https://aff.26march.shop/?utm_medium=2df47d93e8e91e99406b43e1d9446f53b77ff0f8&utm_campaign=2333bal";
 var arlink = "https://aff.26march.shop/?utm_medium=bdd66980840cbaee8b570beb3f0da0732870589b&utm_campaign=37clickj";
-var rblink = "https://aff.26march.shop/?utm_medium=43a7e01b3c3e0d4280eb2f8b7e6cf3e43264201a&utm_campaign=41clickrobi";
+var rblink = "https://uuediuw.com/cl/6063229bd7f83cda?p1=&p2=&source=tiktok&site=10ro";
