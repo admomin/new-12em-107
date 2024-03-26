8e4b2ca1f8313c6dd24b10de6dfcea168e85624b
admomin <67753387+admomin@users.noreply.github.com>
2024-03-26 10:10:50 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://uuediuw.com/cl/19177bd574f54b22?p1=&p2=&source=tiktok&site=100gpca";
+var gplink = "https://uuediuw.com/cl/c9aeba1068303d45?p1=&p2=&source=tiktok&site=gjaad2132";
 var bllink = "https://uuediuw.com/cl/d6140811fb278806?p1=&p2=&source=tiktok&site=10balamar2";
 var arlink = "https://aff.26march.shop/?utm_medium=bdd66980840cbaee8b570beb3f0da0732870589b&utm_campaign=37clickj";
-var rblink = "https://uuediuw.com/cl/6063229bd7f83cda?p1=&p2=&source=tiktok&site=10ro";
+var rblink = "https://flibarox.com/cl/12ba2ad093216113?p1=&p2=&source=instagram&site=4xr";
