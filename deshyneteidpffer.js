ef92c1c7aedb52a01cf98ae54e46261dbc552499
admomin <67753387+admomin@users.noreply.github.com>
2024-03-26 09:08:59 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://aff.26march.shop/?utm_medium=5fdbe1474113ab94841e958e48ce5a8a8015e60e&utm_campaign=31gppp";
-var bllink = "https://aff.26march.shop/?utm_medium=2df47d93e8e91e99406b43e1d9446f53b77ff0f8&utm_campaign=2333bal";
+var gplink = "https://uuediuw.com/cl/19177bd574f54b22?p1=&p2=&source=tiktok&site=100gpca";
+var bllink = "https://uuediuw.com/cl/d6140811fb278806?p1=&p2=&source=tiktok&site=10balamar2";
 var arlink = "https://aff.26march.shop/?utm_medium=bdd66980840cbaee8b570beb3f0da0732870589b&utm_campaign=37clickj";
 var rblink = "https://uuediuw.com/cl/6063229bd7f83cda?p1=&p2=&source=tiktok&site=10ro";
