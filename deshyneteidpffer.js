1ff8b482460e5dfea808478bd76e91e21bc34ef6
admomin <67753387+admomin@users.noreply.github.com>
2024-03-26 02:13:34 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://flibarox.com/cl/6302dbbee8c249bd?p1=&p2=&source=instagram&site=sg4pp";
+var gplink = "https://uuediuw.com/cl/4ae029083ed7e6b7?p1=&p2=&source=tiktok&site=gjaad2132";
 var bllink = "https://flibarox.com/cl/856da60647328fd3?p1=&p2=&source=tiktok&site=m22";
 var arlink = "https://aff.26march.shop/?utm_medium=bdd66980840cbaee8b570beb3f0da0732870589b&utm_campaign=37clickj";
 var rblink = "https://flibarox.com/cl/19ec9ff9a0c613b7?p1=&p2=&source=instagram&site=4xr";
