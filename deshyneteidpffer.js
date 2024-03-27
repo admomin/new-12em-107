2cf4a1e80be74bb1dc036435e94e97e411b6175e
admomin <67753387+admomin@users.noreply.github.com>
2024-03-27 07:11:24 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://pjamekz.com/cl/b67a0371f879fde6?p1=&p2=&source=youtube&site=gmomin";
-var bllink = "https://pjamekz.com/cl/59e49e4cef0ebb9e?p1=&p2=&source=youtube&site=balmomin";
+var gplink = "https://pjamekz.com/cl/7fc662a5770d9a78?p1=&p2=&source=tiktok&site=jiong";
+var bllink = "https://pjamekz.com/cl/475fa09704178c16?p1=&p2=&source=tiktok&site=jionbb";
 var arlink = "https://aff.26march.shop/?utm_medium=c2ff903ebf365d778f57925c2642ea435b4f958e&utm_campaign=newcard";
-var rblink = "https://pjamekz.com/cl/f25a108fcad51db1?p1=&p2=&source=youtube&site=robimumin";
+var rblink = "https://pjamekz.com/cl/d59e504a51d9436c?p1=&p2=&source=tiktok&site=jionrr";
