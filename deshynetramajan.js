5c11c08b831fd877c35881046595340794cec8bc
admomin <67753387+admomin@users.noreply.github.com>
2024-03-27 22:44:36 +0600

    Create deshynetramajan.js

---
+++ deshynetramajan.js
@@ -0,0 +1,4 @@
+var gplink = "https://flibarox.com/cl/2beeb018c139b9d0?p1=&p2=&source=youtube&site=gnet";
+var bllink = "https://flibarox.com/cl/41487b5239fbb62d?p1=&p2=&source=youtube&site=mybne333";
+var arlink = "https://aff.26march.shop/?utm_medium=b374edca4d1b5501cdd70c8bd535ebe897386c8e&utm_campaign=privetoffer";
+var rblink = "https://flibarox.com/cl/e3bffbbe2e245861?p1=&p2=&source=youtube&site=myyta";
