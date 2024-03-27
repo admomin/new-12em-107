6f794c48d86786285f855bfd54430e968ee705b1
admomin <67753387+admomin@users.noreply.github.com>
2024-03-26 23:33:27 -0700

    Update deshyneteidpffer.js

--- deshyneteidpffer.js
+++ deshyneteidpffer.js
@@ -1,4 +1,4 @@
-var gplink = "https://hpmyrik.com/cl/79590f99243846de?p1=&p2=&source=tiktok&site=newfgg";
-var bllink = "https://uuediuw.com/cl/d6140811fb278806?p1=&p2=&source=tiktok&site=10balamar2";
+var gplink = "https://flibarox.com/cl/93538b4dbce0208e?p1=&p2=&source=tiktok&site=soghag";
+var bllink = "https://flibarox.com/cl/59befe5105df6d86?p1=&p2=&source=tiktok&site=soghagbb";
 var arlink = "https://aff.26march.shop/?utm_medium=bdd66980840cbaee8b570beb3f0da0732870589b&utm_campaign=37clickj";
-var rblink = "https://flibarox.com/cl/12ba2ad093216113?p1=&p2=&source=instagram&site=4xr";
+var rblink = "https://flibarox.com/cl/994456a2a24f7850?p1=&p2=&source=tiktok&site=sohagr";
