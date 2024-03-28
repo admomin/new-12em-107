26d07d9d7a6ca2b57426690de418d5ec92f74dab
admomin <67753387+admomin@users.noreply.github.com>
2024-03-28 02:23:12 -0700

    Update deshynetramajan.js

--- deshynetramajan.js
+++ deshynetramajan.js
@@ -1,4 +1,4 @@
-var gplink = "https://aff.26march.shop/?utm_medium=37d807fca151456485c640bc49135f104dbb67f4&utm_campaign=ramadangp";
-var bllink = "https://aff.26march.shop/?utm_medium=166e2744cac3fc380be505877c306bce3a11c5d9&utm_campaign=ramadanbalx";
+var gplink = "https://hpmyrik.com/cl/b9a2a59581325f99?p1=&p2=&source=instagram&site=ramadang";
+var bllink = "https://hpmyrik.com/cl/53a515853227dde6?p1=&p2=&source=instagram&site=ramadanbal";
 var arlink = "https://aff.26march.shop/?utm_medium=7cef705f3053d477ac3a814f2235cab08f0ec2ee&utm_campaign=ramadanarr";
-var rblink = "https://aff.26march.shop/?utm_medium=7cef705f3053d477ac3a814f2235cab08f0ec2ee&utm_campaign=ramadanarr";
+var rblink = "https://hpmyrik.com/cl/2cf4ad8cd9800655?p1=&p2=&source=facebook&site=ramadanr";
